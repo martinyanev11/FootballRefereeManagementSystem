8fceceeb114a87654398a71b1c4319209519021a
Martin Yanev <marrtin@abv.bg>
2023-07-11 14:29:12 +0300

    Changed counter of succuss.js to take starting value from DOM element.

--- FRMS/FootballRefereeManagementSystem.Web/wwwroot/js/success.js
+++ FRMS/FootballRefereeManagementSystem.Web/wwwroot/js/success.js
@@ -1,13 +1,17 @@
-﻿var counter = 5; // Start counter at 5 seconds
-
+﻿// This fuction takes the innerText of HTML element with id = counter and starts a countdown.
+// When time is done, it will redirect to home page
 function updateCounter() {
     var counterElement = document.getElementById("counter");
-    counterElement.innerText = counter;
+    var counter = parseInt(counterElement.innerText);
 
     if (counter === 0) {
         window.location.href = '/Home/Index';
     } else {
         counter--;
+        counterElement.innerText = counter;
         setTimeout(updateCounter, 1000); // 1000 milliseconds = 1 second
     }
 }
+
+// Start the countdown timer
+updateCounter();
