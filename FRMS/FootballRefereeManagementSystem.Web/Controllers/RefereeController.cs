cd07cc7e6aaf0e9a5cc76d0c4cf330b710df6b78
Martin Yanev <marrtin@abv.bg>
2023-07-17 16:06:26 +0300

    Added RefereeController.

---
+++ FRMS/FootballRefereeManagementSystem.Web/Controllers/RefereeController.cs
@@ -0,0 +1,12 @@
+﻿namespace FootballRefereeManagementSystem.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class RefereeController : BaseController
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
