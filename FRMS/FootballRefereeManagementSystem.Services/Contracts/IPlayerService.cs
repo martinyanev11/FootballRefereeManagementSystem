a4445568d75124f2abf743ce6d58e5d181c90fc4
Martin Yanev <marrtin@abv.bg>
2023-07-26 16:29:52 +0300

    Player service added.

--- FRMS/FootballRefereeManagementSystem.Services/Contracts/IPlayerService.cs
+++ FRMS/FootballRefereeManagementSystem.Services/Contracts/IPlayerService.cs
@@ -1,6 +1,11 @@
 ﻿namespace FootballRefereeManagementSystem.Services.Contracts
 {
+    using FootballRefereeManagementSystem.Web.ViewModels.Player;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
     public interface IPlayerService
     {
+        Task<IEnumerable<PlayerDetailsViewModel>> GetTeamPlayersForSeasonAsync(int id, int seasonId);
     }
 }
