adc6d8c659bb9f1f213bcc58c3ef9b65a161b4aa
Martin Yanev <marrtin@abv.bg>
2023-08-13 10:54:31 +0300

    More tests for services.

--- FRMS/FootballRefereeManagementSystem.Services.Tests/Tests/SeasonServiceTests.cs
+++ FRMS/FootballRefereeManagementSystem.Services.Tests/Tests/SeasonServiceTests.cs
@@ -67,5 +67,27 @@
             // Assert
             Assert.That(result, Is.EqualTo(expected));
         }
+
+        [Test]
+        public async Task GetCurrentSeasonStatisticsAsync_ShouldReturnCorrectData()
+        {
+            // Arrange
+            string expectedSeasonDescription = "2023/24";
+            int expectedRegisteredTeamsCount = 8; // Seeded 8 TeamSeason entities with seasonId = 2.
+            int expectedRegisteredPlayersCount = 88; // Seeded 88 players in total. Each team has 11 PlayerTeamSeason entities
+            int expectedMatchesPlayed = 0; // No games should be played as the new season hasn't started yet.
+            int expectedTotalMatchesCount = 56; // 8 teams play each other -> 8 * 7 (because teams can't play themself)
+
+            // Act
+            SeasonStatisticsViewModel seasonStats = await this.seasonService.GetCurrentSeasonStatisticsAsync();
+
+            // Assert
+            Assert.That(seasonStats.SeasonDescription, Is.EqualTo(expectedSeasonDescription));
+            Assert.That(seasonStats.RegisteredTeamsCount, Is.EqualTo(expectedRegisteredTeamsCount));
+            Assert.That(seasonStats.RegisteredTeamsCount, Is.EqualTo(expectedRegisteredTeamsCount));
+            Assert.That(seasonStats.RegisteredPlayersCount, Is.EqualTo(expectedRegisteredPlayersCount));
+            Assert.That(seasonStats.MatchesPlayed, Is.EqualTo(expectedMatchesPlayed));
+            Assert.That(seasonStats.TotalMatchesCount, Is.EqualTo(expectedTotalMatchesCount));
+        }
     }
 }
---
+++ FRMS/FootballRefereeManagementSystem.Services.Tests/Tests/TeamServiceTests.cs
@@ -0,0 +1,85 @@
+﻿namespace FootballRefereeManagementSystem.Services.Tests.Tests
+{
+    using Contracts;
+    using Web.ViewModels.Team;
+
+    public class TeamServiceTests : TestDatabaseFixture
+    {
+        private ITeamService teamService;
+
+        [SetUp]
+        public async new Task InitializeDatabaseForTests()
+        {
+            await base.InitializeDatabaseForTests();
+
+            this.teamService = new TeamService(dbContext);
+        }
+
+        [Test]
+        public async Task GetAllTeamsAsync_NoFilters_ShouldReturnAllActiveTeams()
+        {
+            // Arrange
+            int expectedTeamsCount = 17;
+            TeamAdminQueryModel queryModel = new TeamAdminQueryModel()
+            {
+                SearchString = string.Empty,
+                Zone = null,
+            };
+
+            // Act
+            IEnumerable<TeamAdminViewModel> teams = 
+                await this.teamService.GetAllTeamsAsync(queryModel);
+
+            // Arrange
+            Assert.That(teams.Count(), Is.EqualTo(expectedTeamsCount));
+        }
+
+        [Test]
+        public async Task GetAllTeamsAsync_ZoneFilter_ShouldReturnTeamsInSelectedZone()
+        {
+            // Arrange
+            int expectedTeamsCount = 9;
+            string zoneFilter = "Плевен Изток";
+            TeamAdminQueryModel queryModel = new TeamAdminQueryModel()
+            {
+                SearchString = string.Empty,
+                Zone = zoneFilter,
+            };
+
+            // Act
+            IEnumerable<TeamAdminViewModel> teams =
+                await this.teamService.GetAllTeamsAsync(queryModel);
+
+            // Arrange
+            Assert.That(teams.Count(), Is.EqualTo(expectedTeamsCount));
+        }
+
+        [Test]
+        public async Task CheckIfTeamIsAlreadyRegistered_RegisteredId_ShouldReturnTrue()
+        {
+            // Arrange
+            int teamId = 1;
+            int seasonId = 2;
+
+            // Act
+            bool result = await this.teamService.CheckIfTeamIsAlreadyRegistered(teamId, seasonId);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task CheckIfTeamIsAlreadyRegistered_NotRegisteredId_ShouldReturnFalse()
+        {
+            // Arrange
+            int teamId = 10;
+            int seasonId = 2;
+
+            // Act
+            bool result = await this.teamService.CheckIfTeamIsAlreadyRegistered(teamId, seasonId);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
